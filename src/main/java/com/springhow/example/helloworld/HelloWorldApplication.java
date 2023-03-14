fd3acb1c7927c547b12b84e7f95ee0756fa14249
mujee33b <119735686+mujee33b@users.noreply.github.com>
2023-03-14 09:13:14 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "ALLAH is always with me everytime";
+        return "ALLAH is always with me everytimes";
     }
 
 }
